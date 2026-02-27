c0967215d0b84bbae2257ba7b135ed6e1282632d
Vasilis Kroupas <122830585+PuzzleGamesByVK@users.noreply.github.com>
2026-02-27 09:31:02 +0200

    VERSION 0.0.2
    importmap - Update THREE.js and OrbitControls import paths

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Stages6of7, inicialXY, ABCcds } from './stages.js';
 
 const { createApp, ref, computed, onMounted } = Vue;
 
