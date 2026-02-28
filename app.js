ff2aca1250cbb7dfea7c278899c0c2a379f3c660
Vasilis Kroupas <122830585+PuzzleGamesByVK@users.noreply.github.com>
2026-03-01 01:06:33 +0200

    VERSION 0.1.1
    Comment out the stageName computed property. duble.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ createApp({
 
         let scene, camera, renderer, raycaster, pointer;
 
-        const stageName = computed(() => LaslosLeapRaw[currentStageIdx.value][1]);
+        //const stageName = computed(() => LaslosLeapRaw[currentStageIdx.value][1]);
         const currentPar = computed(() => LazloPars[currentStageIdx.value]);
 
         const loadStage = (idx) => {
